676153bafa54689954ac5302cb8049e07b03654d
Viren <bhanderiviren@gmail.com>
2026-03-16 14:48:36 -0500

    Finished the model and made app.py file that can be used to test new inputs

--- app.py
+++ app.py
@@ -1,15 +1,38 @@
-import re
-import streamlit as st
 import pickle
+import nltk
+import string
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+ps = PorterStemmer()
+
+# This must exist before unpickling transform.pkl, because the pickle refers to
+# __main__.transform_text when it was created.
 
 
-# This must exist before unpickling transform.pkl,
-# because the pickled object expects a `transform_text` name
-# to be available in the module it is loaded in.
 def transform_text(text: str) -> str:
     text = text.lower()
-    text = re.sub(r"[^a-z0-9\s]", "", text)
-    return text
+    text = nltk.word_tokenize(text)
+
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words("english") and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
 
 
 # Load the model and vectorizer
@@ -17,25 +40,18 @@ transform_text = pickle.load(open("transform.pkl", "rb"))
 tfidf = pickle.load(open("vectorizer.pkl", "rb"))
 model = pickle.load(open("model.pkl", "rb"))
 
-st.title("Email/SMS Spam Classifier")
 
-input_text = st.text_input("Enter the text to classify")
-
-
-# Create a function to predict the label of the input text
 def predict(text):
-    # Transform the input text
     transformed_text = transform_text(text)
-    # Vectorize the transformed text
-    vectorized_text = tfidf.transform([transformed_text])
-    # Predict the label
+    vectorized_text = tfidf.transform([transformed_text]).toarray()
     prediction = model.predict(vectorized_text)[0]
     return prediction
 
 
-if st.button("Predict"):
-    result = predict(input_text)
-    if result == 1:
-        st.header("Spam.")
-    else:
-        st.header("Not Spam.")
+input_text = input("Enter a message to classify as spam or not spam: ")
+
+result = predict(input_text)
+if result == 1:
+    print("Spam.")
+else:
+    print("Not Spam.")
